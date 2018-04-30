6796509f59a4f896a20241d8ca494b106c5d1f43
YannickDieter <dieter@physik.uni-bonn.de>
2018-04-30 10:27:03 +0200

    MAINT: cleaner way to store skipped trigger counter

--- pytlu/tlu.py
+++ pytlu/tlu.py
@@ -222,9 +222,9 @@ class Tlu(Dut):
         self.meta_data_table.row['timestamp_start'] = data_tuple[1]
         self.meta_data_table.row['timestamp_stop'] = data_tuple[2]
         self.meta_data_table.row['error'] = data_tuple[3]
+        self.meta_data_table.row['skipped_triggers'] = data_tuple[4]
         self.meta_data_table.row['data_length'] = len_raw_data
         self.meta_data_table.row['index_start'] = total_words
-        self.meta_data_table.row['skipped_triggers'] = self.fifo_readout.get_data_tlu_skipped_trigger_count()
         total_words += len_raw_data
         self.meta_data_table.row['index_stop'] = total_words
         self.meta_data_table.row.append()
