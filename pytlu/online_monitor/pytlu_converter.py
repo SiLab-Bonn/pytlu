98838a08c7c198c68ca8307fc03f6683bf3190ad
YannickDieter <dieter@physik.uni-bonn.de>
2019-06-27 10:41:03 +0200

    BUG: fix data deserialization

--- pytlu/online_monitor/pytlu_converter.py
+++ pytlu/online_monitor/pytlu_converter.py
@@ -56,7 +56,7 @@ class PyTLU(Transceiver):
                 shape = self.meta_data.pop('shape')
                 if self.meta_data:
                     try:
-                        raw_data_array = np.frombuffer(buffer(data), dtype=dtype).reshape(shape)
+                        raw_data_array = np.frombuffer(data, dtype=dtype).reshape(shape)
                         return raw_data_array
                     except (KeyError, ValueError, TypeError):  # KeyError happens if meta data read is omitted; ValueError if np.frombuffer fails due to wrong sha
                         return None
